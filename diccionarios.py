343cfcdd0b9e5d33aceb585dcb37ee46a9698778
Jair <jairaceves56@gmail.com>
2023-05-30 08:31:41 -0600

    Diccionarios en Python

---
+++ diccionarios.py
@@ -0,0 +1,23 @@
+# Diccionario
+# Es una estructura de datos de campo:/valor
+
+persona1 = {"nombre":"Maria Georgina",
+           "edad": 30,
+           "altura": 1.56,
+           "genero": "Femenino",
+           "esttus": True,
+           "WhatsApp": 33478483930,
+           "correo": "mariageo89@gmail.com"
+        }
+
+persona2 = {"nombre":"Jonathan Gonzalez",
+           "edad": 28,
+           "altura": 1.86,
+           "genero": "Masculino",
+           "esttus": True,
+           "WhatsApp": 33477546930,
+           "correo": "joniThan49@gmail.com"
+        }
+
+
+print(persona1)
