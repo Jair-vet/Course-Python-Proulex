06f19bc582862869fac267c719e96a65482b2901
Jair <jairaceves56@gmail.com>
2023-06-05 08:33:41 -0600

    Extraemos los datos y los guardamos en una variable

---
+++ pandas/pd3.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Data Frame: es una estructura de dos dimensiones
+# Podriamos decir que es una tabla (Filas, Columnas)
+
+# Cargar el Archivo al DataFrame
+
+df = pd.read_csv("/Users/jairaceves/Documents/Curso Python/pandas/datos.csv")
+print(df)
