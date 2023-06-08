3d501b59d577bdf6b7a3d028142ee829c6129fc6
Jair <jairaceves56@gmail.com>
2023-06-08 07:54:11 -0600

    Creamos el primer ejemplo de una Tortuga

---
+++ ejemplo_tortuga.py
@@ -0,0 +1,50 @@
+import turtle
+import time
+
+# Figura 1
+turtle.color("#33FFF8")
+turtle.width(5)
+turtle.speed(12)
+
+for t in range(10):
+    turtle.circle(40)
+    turtle.right(35)
+
+# Figura 2
+turtle.color("#33FF09")
+turtle.width(5)
+turtle.speed(12)
+
+for t in range(10):
+    turtle.circle(60)
+    turtle.right(35)
+
+# Figura 2
+turtle.color("#C51D1D")
+turtle.width(5)
+turtle.speed(12)
+
+for t in range(10):
+    turtle.circle(90)
+    turtle.right(35)
+
+# Figura 4
+turtle.color("#C8E20D")
+turtle.width(5)
+turtle.speed(12)
+
+for t in range(10):
+    turtle.circle(100)
+    turtle.right(35)
+
+# Figura 5
+turtle.color("#853589")
+turtle.width(5)
+turtle.speed(12)
+
+for t in range(10):
+    turtle.circle(10)
+    turtle.right(35)
+
+
+time.sleep(5)
